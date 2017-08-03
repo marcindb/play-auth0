b194612cab24b8b0f060b775821a6a076666027e
marcindb <mdebow@gmail.com>
2017-08-03 17:28:46 +0200

    Reformat code

--- play-auth0/src/main/scala/pl/ekodo/play/auth0/Secured.scala
+++ play-auth0/src/main/scala/pl/ekodo/play/auth0/Secured.scala
@@ -8,14 +8,15 @@ import play.api.mvc._
 import scala.concurrent.{ ExecutionContext, Future }
 import scala.util.control.NonFatal
 
-class Secured @Inject() (auth0Secured: Auth0Secured, val parser: BodyParsers.Default)(implicit val executionContext: ExecutionContext)
+class Secured @Inject() (auth0Secured: Auth0Secured, val parser: BodyParsers.Default)
+                        (implicit val executionContext: ExecutionContext)
     extends ActionBuilder[VerifiedRequest, AnyContent] with ActionRefiner[Request, VerifiedRequest] {
   override def refine[A](input: Request[A]): Future[Either[mvc.Results.Status, VerifiedRequest[A]]] =
     input.headers.get("Authorization").map { auth =>
       auth0Secured.validate(auth).map { subject =>
         Right(new VerifiedRequest(subject, input))
       }.recover {
-        case NonFatal(e) => Left(Results.Forbidden)
+        case NonFatal(_) => Left(Results.Forbidden)
       }
     }.getOrElse(Future.successful(Left(Results.Forbidden)))
 }
